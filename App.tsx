c54a6b744a7a31fbfc5a5b71f7cdfab7a43d2714
TheHumphrey <fernandob.delima@outlook.com>
2022-08-11 11:05:13 -0300

    chore: install expo-fonts and more

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
+import { THEME } from './src/styles/theme'
+
+import { NativeBaseProvider } from 'native-base'
+
 import { SignIn } from './src/screens'
 
 export default function App() {
   return (
-    <SignIn />
+    <NativeBaseProvider theme={THEME}>
+      <SignIn />
+    </NativeBaseProvider>
   );
 }
