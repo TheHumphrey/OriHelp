09550c59abb8c4147d7c4712911b945c8eeae15c
TheHumphrey <fernandob.delima@outlook.com>
2022-08-11 11:14:40 -0300

    chore: updata status bar and use font hooks

--- App.tsx
+++ App.tsx
@@ -1,15 +1,23 @@
-import React from 'react'
+
+import { NativeBaseProvider, StatusBar } from 'native-base'
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 import { THEME } from './src/styles/theme'
 
-import { NativeBaseProvider } from 'native-base'
-
 import { SignIn } from './src/screens'
+import { Loading } from './src/components/templates'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
     <NativeBaseProvider theme={THEME}>
-      <SignIn />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? (<SignIn />) : (<Loading />)}
     </NativeBaseProvider>
   );
 }
