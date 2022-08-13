9cce0dc9b0e065e54945cfa477f6274eced569d8
TheHumphrey <fernandob.delima@outlook.com>
2022-08-13 19:23:05 -0300

    chore: starte create home

---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,57 @@
+import { SignOut } from 'phosphor-react-native'
+
+import {
+  HStack,
+  VStack,
+  IconButton,
+  useTheme,
+  Text,
+  Heading
+} from 'native-base'
+
+import Logo from '../../assets/logo_secondary.svg'
+
+export const Home = () => {
+  const { colors } = useTheme()
+
+  return (
+    <VStack flex={1} pb={6} bg="gray.700">
+      <HStack
+        w="full"
+        justifyContent="space-between"
+        alignItems="center"
+        bg="gray.700"
+        pt={12}
+        pb={5}
+        px={6}
+      >
+        <Logo />
+
+        <IconButton
+          icon={<SignOut size={26} color={colors.gray[300]} />}
+        />
+
+      </HStack>
+
+      <VStack flex={1} px={6}>
+        <HStack
+          w="full"
+          mt={6}
+          mb={4}
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Heading color="gray.100">
+            Meus chamados
+          </Heading>
+
+          <Text color="gray.200">
+            3
+          </Text>
+        </HStack>
+      </VStack>
+
+
+    </VStack>
+  );
+}
