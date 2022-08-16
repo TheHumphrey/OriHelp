059214d3e308a60f3fff47d489e8bffff0c58d96
TheHumphrey <fernandob.delima@outlook.com>
2022-08-16 10:04:07 -0300

    chore: datails params

--- src/screens/Details/Details.tsx
+++ src/screens/Details/Details.tsx
@@ -1,7 +1,17 @@
-import { VStack } from 'native-base';
-import { Header } from '../../components/molecules';
+import { VStack, Text } from 'native-base'
+import { useRoute } from '@react-navigation/native'
+
+import { Header } from '../../components/molecules'
+
+type RouteParams = {
+  orderId: string
+}
 
 export const Details = () => {
+  const route = useRoute()
+
+  const { orderId } = route.params as RouteParams
+
   return (
     <VStack flex={1} bg="gray.700">
       <Header title='solicitação' />
